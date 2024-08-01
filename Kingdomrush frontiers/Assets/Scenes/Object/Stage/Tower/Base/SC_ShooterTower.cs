8457c67dfaee65931667f44e62b5cbd485504198
승민 황 <iqmin11@gmail.com>
2024-08-01 12:36:37 +0900

    황승민 : 함수명 수정

--- Kingdomrush frontiers/Assets/Scenes/Object/Stage/Tower/Base/SC_ShooterTower.cs
+++ Kingdomrush frontiers/Assets/Scenes/Object/Stage/Tower/Base/SC_ShooterTower.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 abstract public class SC_ShooterTower : SC_BaseShootingTower
 {
-    abstract protected void TransitionTargetInfoToShooter();
+    abstract protected void TransmissionTargetInfoToShooter();
     protected override void CalTargetPos()
     {
         base.CalTargetPos();
-        TransitionTargetInfoToShooter();
+        TransmissionTargetInfoToShooter();
     }
-
 }
